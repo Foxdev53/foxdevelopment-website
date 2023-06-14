eb6db95e1815bd04c15e68b3daf9cc6e6a250eef
Coolcat34670 <125183710+Coolcat34670@users.noreply.github.com>
2023-06-14 04:09:23 -0500

    Updated Files {6/14/23}

--- footer.js
+++ footer.js
@@ -21,15 +21,7 @@ class Footer extends HTMLElement {
       <footer>
       <center>
       This Site Was Made by Fox Development.   
-      <center>
-<div>
-      <right>
-      <a href="https://foxdevelopment.netlify.app/public/sitemap.txt" class="fa fa-sitemap"></a>
-      <a href="https://twitter.com/Foxdev53" class="fa fa-twitter"></a>
-      <a href="https://github.com/Foxdev53/foxdevelopment-website" class="fa fa-github"></a>
-      <a href="https://youtube.com/@Foxdev53" class="fa fa-youtube"></a>
-      </right>
-      </div>   
+      <center>  
       </footer>
       
 
